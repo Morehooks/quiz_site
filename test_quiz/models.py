c49a5e24b563bc4cf497f8a8efee8c3101ea4bc1
jake <jakelwatson86@gmail.com>
2017-01-10 20:39:34 +0000

    Add initial Section, Question and Response models

--- test_quiz/models.py
+++ test_quiz/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Section(models.Model):
+    section_text = models.CharField(max_length=255)
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=255)
+    section = models.ForeignKey('Section', on_delete=models.CASCADE)
+
+
+class Response(models.Model):
+    response_text = models.CharField(max_length=255)
+    response_value = models.IntegerField()
+    question = models.ForeignKey('Question', on_delete=models.CASCADE)
