ea2300129c4af67c0fac80cc995d6399ae937e63
jake <jakelwatson86@gmail.com>
2017-01-10 22:01:29 +0000

    Added settings.py

--- test_quiz/admin.py
+++ test_quiz/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Section, Question, Response
 
-# Register your models here.
+
+class ResponseInline(admin.StackedInline):
+    model = Response
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text', 'section']}),
+    ]
+    inlines = [ResponseInline]
+
+
+admin.site.register(Section)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Response)
--- test_quiz/models.py
+++ test_quiz/models.py
@@ -4,15 +4,24 @@ from django.db import models
 class Section(models.Model):
     section_text = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.section_text
+
 
 class Question(models.Model):
     question_text = models.CharField(max_length=255)
     section = models.ForeignKey('Section', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.question_text
 
 
 class Response(models.Model):
     response_text = models.CharField(max_length=255)
     response_value = models.IntegerField()
     question = models.ForeignKey('Question', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.response_text
 
 
