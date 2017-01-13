f46c2ec6763756eb35cf7d2158e2528a3a8a502c
jake <jakelwatson86@gmail.com>
2017-01-13 20:37:46 +0000

    Added settings.py

--- test_quiz/models.py
+++ test_quiz/models.py
@@ -5,15 +5,31 @@ class Section(models.Model):
     section_text = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.section_text
+        return str(self.id) + ': ' + self.section_text
+
+
+class Page(models.Model):
+    page_header = models.TextField()
+    section = models.ForeignKey('Section', on_delete=models.CASCADE)
+
+
+class SubPage(models.Model):
+    sub_page_header = models.TextField()
+    page = models.ForeignKey('Page', on_delete=models.CASCADE)
 
 
 class Question(models.Model):
+    question_seq = models.IntegerField(default=100)
     question_text = models.CharField(max_length=255)
-    section = models.ForeignKey('Section', on_delete=models.CASCADE)
+    question_type = models.CharField(max_length=255)
+    sub_page = models.ForeignKey('SubPage', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.question_text
+
+    #def default_question_seq_value(self):
+       # if
+        #return self.objects.all().aggregate(models.Max(self.question_seq)) + 100
 
 
 class Response(models.Model):
