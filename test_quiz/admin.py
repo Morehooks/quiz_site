55d23a9c24d9faf5098e03f8a23a49f861e4e6a6
jake <jakelwatson86@gmail.com>
2017-01-15 17:37:39 +0000

    Added a method to admin.py to adjust all the admin.py textboxes to a larger size

--- test_quiz/admin.py
+++ test_quiz/admin.py
@@ -1,19 +1,30 @@
 from django.forms import TextInput, Textarea
+from django.db import models
 from django.contrib import admin
 from .models import Section, Page, SubPage, Question, Response
 
 
+def get_form_overrides():
+    return {
+        models.CharField: {'widget': Textarea(attrs={'rows': 1, 'cols': 255})},
+        models.TextField: {'widget': Textarea(attrs={'rows': 10, 'cols': 200})},
+    }
+
+
 class ResponseInline(admin.StackedInline):
+    formfield_overrides = get_form_overrides()
     model = Response
     extra = 1
 
 
 class QuestionInline(admin.StackedInline):
+    formfield_overrides = get_form_overrides()
     model = Question
     extra = 1
 
 
 class SubPageAdmin(admin.ModelAdmin):
+    formfield_overrides = get_form_overrides()
     fieldsets = [
         (None, {'fields': ['sub_page_header', 'sub_page_seq', 'page']}),
     ]
@@ -21,6 +32,7 @@ class SubPageAdmin(admin.ModelAdmin):
 
 
 class QuestionAdmin(admin.ModelAdmin):
+    formfield_overrides = get_form_overrides()
     fieldsets = [
         (None,               {'fields': ['question_text', 'question_seq', 'question_type', 'sub_page']}),
     ]
